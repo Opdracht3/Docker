cac18d89e01283bdfea3e7ac4e0e17b0c821ca5c
Husamay <husamay07@gmail.com>
2017-10-30 18:34:57 +0100

    changed jenkins file

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker run -d -p 8080:8080 -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v /home/husamay/workspace/docker/jenkins_home:/var/jenkins_home test
+sudo docker run -d -p 80:80 -p 8080:8080 -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v /home/husamay/workspace/rps-pipeline/jenkins_home:/var/jenkins_home test
